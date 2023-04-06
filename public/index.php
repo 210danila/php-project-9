599ffbe3db4c3c0b798fedf8758353b9e10a7e8f
210danila <210danila@gmail.com>
2023-04-06 23:36:57 +0300

    Initial setting

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+$autoloadPath1 = __DIR__ .  '/../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write("Hello, world!");
+    return $response;
+});
+
+$app->run();
