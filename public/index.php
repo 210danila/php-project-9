7e7fb2c970039008d5369fa8c2e5260364d53c95
210danila <210danila@gmail.com>
2023-04-19 21:21:33 +0300

    Add first template for the main page, add renderer to index.php

--- public/index.php
+++ public/index.php
@@ -14,12 +14,18 @@ if (file_exists($autoloadPath1)) {
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function (Request $request, Response $response) {
-    $response->getBody()->write("Hello, world!");
-    return $response;
+    return $this->get('renderer')->render($response, "index.html", []);
 });
 
 $app->run();
