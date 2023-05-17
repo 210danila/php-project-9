a87823df1efa0abd83d15cd95884b39d8ad64026
210danila <210danila@gmail.com>
2023-05-17 20:36:01 +0300

    Small fixes

--- app/Connection.php
+++ app/Connection.php
@@ -17,7 +17,7 @@ final class Connection
      */
     public function connect()
     {
-        $databaseUrl = parse_url($_ENV('DATABASE_URL'));
+        $databaseUrl = parse_url($_ENV['DATABASE_URL']);
         if ($databaseUrl === false) {
             throw new \Exception("Error reading database configuration file");
         }
