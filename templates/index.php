7b706cf416f72cac13e926c6ec34a837045bcc37
210danila <210danila@gmail.com>
2023-05-30 03:29:04 +0300

    Another try2

--- templates/index.php
+++ templates/index.php
@@ -26,7 +26,7 @@
                 <?php if (empty($errors)) : ?>
                   <input type="text" name="url[name]" value="" class="form-control form-control-lg" placeholder="https://www.example.com">
                 <?php else : ?>
-                  <?php if (empty($urlName)) : ?>
+                    <?php if (empty($urlName)) : ?>
                     <input type="text" name="url[name]" value="" class="form-control form-control-lg is-invalid" placeholder="https://www.example.com">
                   <?php else : ?>
                     <input type="text" name="url[name]" value=<?= htmlspecialchars($urlName) ?> class="form-control form-control-lg is-invalid" placeholder="https://www.example.com">
